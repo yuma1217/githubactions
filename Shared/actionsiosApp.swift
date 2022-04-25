caadaa2eda0a3f76d734b5efb29180e82e6e551f
yuma1217 <yu.matsumura1217@gmail.com>
2022-04-25 15:26:09 +0900

    Initial Commit

---
+++ Shared/actionsiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  actionsiosApp.swift
+//  Shared
+//
+//  Created by test on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct actionsiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
